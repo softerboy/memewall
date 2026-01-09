ca949247ff2728e3d5e68ea71efc80bae80b3148
nightowl <fn.umarov@gmail.com>
2026-01-09 22:10:49 +0500

    feat: update app favicon to use external URL
    - Replace the local favicon with an external emoji-based favicon sourced from `fav.farm`.
    - Comment out the SVG favicon link for potential future use.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,8 +32,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="https://fav.farm/%F0%9F%98%8E" sizes="any">
+{{--        <link rel="icon" href="/favicon.svg" type="image/svg+xml">--}}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
